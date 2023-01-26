9f671ecc7c06d14662fe4744bf0eae44c214899f
gabycavalli <gabrielcavalli@gmail.com>
2023-01-26 16:30:11 -0300

    Mas ejercicios

--- main.cpp
+++ main.cpp
@@ -58,18 +58,82 @@ int main()
 
     /*5. Dados dos valores numéricos, informar cual es el mayor y cual es el menor o si ambos valores son iguales emitir un mensaje.*/
 
-    int num3 = 18;
-    int num4 = 15;
+    // int num3 = 18;
+    // int num4 = 15;
 
-    if(num3>num4)
-        cout<<"el mayor es: "<<num3<<endl;
-    else
-        if(num3<num4)
-            cout<<"el mayor es: "<<num4<<endl;
-    else
-        cout<<"Los numeros son iguales"<<endl;
+    // if(num3>num4)
+    //     cout<<"el mayor es: "<<num3<<endl;
+    // else
+    //     if(num3<num4)
+    //         cout<<"el mayor es: "<<num4<<endl;
+    // else
+    //     cout<<"Los numeros son iguales"<<endl;
 
     // 6. Dadas dos fechas, informar cual es la más reciente. Determinar cuales deben ser los datos de entrada y en que formato los debe ingresar el usuario.
+
+    // int date1 = 19780516;
+    // int date2 = 19940523;
+
+    // if(date1>date2)
+    //     cout<<"La fecha mayor es : "<<date1<<endl;
+    // else
+    //     if(date1<date2)
+    //         cout<<"La fecha mayor es: "<<date2<<endl;
+    // else
+    //     cout<<"Las fechas son iguales"<<endl;
+
+    // 7. Dado un triángulo representado por sus lados lado1, lado2 y lado3, determinar e indicar según corresponda: “equilátero”, “isósceles” o “escálenos”. 
+
+    // int lado1 = 28;
+    // int lado2 = 23;
+    // int lado3 = 22;
+
+    // if(lado1==lado2 && lado3==lado1)
+    //      cout<<"El triangulo es Equilatero"<<endl;
+    // else
+    //     if(lado1==lado2 && lado3 != lado1)
+    //         cout<<"El triangulo es Isosceles"<<endl;
+    // else
+    //      cout<<"El triangulo es Escaleno"<<endl;
+
+    // 8. Dados un mes y el año al que corresponde, informar cuantos días tiene el mes.
+
+    // int mes = 2;
+
+    // switch (mes)
+    // {
+
+    // case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+    //     printf("El Mes ingresado tiene 31 dias");
+    //     break;
+    // case 4: case 6: case 9: case 11:
+    //     printf("El Mes ingresado tiene 30 dias");
+    //     break;
+    // case 2: printf("El Febrero puede tener 28 o 29 dias si el anio es bisiesto");
+        
+    // default:
+    //     printf("Ingreso un mes incorrecto");
+    //     break;
+    // }
+
+    //9. Se ingresa una edad, mostrar por pantalla alguna de las siguientes leyendas: 
+        // “menor” si la edad es menor o igual a 12
+        // “cadete” si la edad está comprendida entre 13 y 18,
+        // “juvenil” si la edad es mayor que 18 y no supera los 26, y
+        // “mayor” si no cumple ninguna de las condiciones anteriores. 
+
+        int edad = 27;
+
+        if(edad<=12) 
+            cout<<"La edad ingresada corresponde a un Menor"<<endl;
+        if (edad>=13 && edad<=18)
+            cout<<"La edad ingresada corresponde a un cadete"<<endl;
+        if(edad>=19 && edad<=26)
+            cout<<"La edad ingresada corresponde a un Juvenil"<<endl;
+        if(edad>26)
+            cout<<"La edad ingresada corresponde a un Mayor"<<endl;         
+   
+
 
     return 0;
 }
